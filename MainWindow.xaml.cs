3bb14b05b5553c633ff39d9c8b5c3bfc2df86fd8
hajindvlp <harryhajinchung@gmail.com>
2019-10-12 23:20:38 +0900

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Margit
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Render()
+        {
+            string InputContent = InputTb.Text;
+            string HtmlContent = MarkdownRender.Md2HTML(InputContent);
+
+            if (RenderWb != null)
+                RenderWb.NavigateToString(HtmlContent);
+        }
+
+        private void inputTextChangedEventHandler(object sender, TextChangedEventArgs args)
+        {
+            Render();
+        }
+
+        private void RenderWbNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            if (e.Uri != null)
+                Render();
+        }
+        private void NewContent(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new MainWindow();
+            newWindow.Show();
+        }
+
+        private void OpenContent(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Markdown file (*.md)|*.md"
+            };
+            if (openFileDialog.ShowDialog() == true)
+                InputTb.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void SaveContent(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Markdown file (*.md)|*.md"
+            };
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, InputTb.Text);
+        }
+
+        private void ExitClicked(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SaveAsContent(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Markdown file (*.md)|*.md";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, InputTb.Text);
+        }
+    }
+}
---
+++ MarkdownRender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Markdig;
+
+namespace Margit
+{
+    static class MarkdownRender
+    {
+        private static readonly string styleLink = "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css'>";
+        private static readonly string wrapperStart = "<div class='markdown-body'>";
+        private static readonly string wrapperEnd = "</div>";
+
+        static public string Md2HTML(string MdContent)
+        {
+            string HtmlContent;
+            string RenderContent;
+
+            HtmlContent = Markdown.ToHtml(MdContent);
+            RenderContent = styleLink + wrapperStart + HtmlContent + wrapperEnd;
+            return RenderContent;
+        }
+    }
+}
